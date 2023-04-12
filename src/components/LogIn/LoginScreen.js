bba20319d43d945b1c387ddc98efa7c186405d50
yao-le <le.yao@northeastern.edu>
2023-04-12 10:21:04 -0400

    add radio buttons on LoginScreen

--- src/components/LogIn/LoginScreen.js
+++ src/components/LogIn/LoginScreen.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../style/login-screen.css";
 
 const LoginScreen = () => {
+    const [role, setRole] = useState("listener");
+
     const handleSubmit = (event) => {
         event.preventDefault();
         // Add login logic
@@ -30,11 +32,55 @@ const LoginScreen = () => {
                         />
                     </div>
 
+                    <div className="wd-login-form-group">
+                        <label className="wd-login-form-label">Role</label>
+                        <div className="wd-login-radio-group">
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="listener"
+                                    name="role"
+                                    value="listener"
+                                    checked={role === "listener"}
+                                    onChange={(e) => setRole(e.target.value)}
+                                />
+                                <label htmlFor="listener" className="wd-login-radio-label">
+                                    Listener
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="publisher"
+                                    name="role"
+                                    value="publisher"
+                                    checked={role === "publisher"}
+                                    onChange={(e) => setRole(e.target.value)}
+                                />
+                                <label htmlFor="publisher" className="wd-login-radio-label">
+                                    Publisher
+                                </label>
+                            </div>
+                            <div>
+                                <input
+                                    type="radio"
+                                    id="admin"
+                                    name="role"
+                                    value="admin"
+                                    checked={role === "admin"}
+                                    onChange={(e) => setRole(e.target.value)}
+                                />
+                                <label htmlFor="admin" className="wd-login-radio-label">
+                                    Admin
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+
                     <button type="submit" className="wd-login-button mt-4">
                         Log In
                     </button>
                 </form>
-
             </div>
         </div>
     );
