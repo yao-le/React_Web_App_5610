6f649a30525a838e88b0f50eb66ff736cd964d1b
yao-le <le.yao@northeastern.edu>
2023-04-15 19:54:20 -0400

    change comment form layout

--- src/components/Comment/CommentForm.js
+++ src/components/Comment/CommentForm.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import '../../style/review-form.css';
 import "../../style/comment.css";
+import {useSelector} from "react-redux";
 
-const CommentForm = ({ submitReview }) => {
+const CommentForm = ({submitReview}) => {
+    const {currentUser} = useSelector((state) => state.user);
+
     const [reviewText, setReviewText] = useState('');
 
     const handleSubmit = (e) => {
@@ -14,25 +17,36 @@ const CommentForm = ({ submitReview }) => {
 
     return (
         <form className="wd-review-form w-75" onSubmit={handleSubmit}>
-            <div className="mb-3">
-                {/*<label htmlFor="reviewText" className="form-label wd-review-form-label">*/}
-                {/*    Add a comment:*/}
-                {/*</label>*/}
-                <textarea
+            <div className="d-flex flex-column">
+                {
+                    currentUser &&
+                    <>
+                        <div className="mb-1 text-muted">Commenting as</div>
+                        <div className="d-flex flex-row align-items-center">
+                            <img className="rounded-circle me-2" width="50px" height="50px"
+                                 src={currentUser.portrait} alt="avatar"/>
+                            <div className="wd-commenterName">{currentUser.name}</div>
+                        </div>
+                    </>
+                }
+                <div className="mb-2 mt-1">
+                    <textarea
                     className="form-control wd-review-form-textarea"
                     id="reviewText"
                     value={reviewText}
                     onChange={(e) => setReviewText(e.target.value)}
                     rows="3"
                     placeholder="Add a comment..."
-                ></textarea>
-            </div>
-            <div className="text-end">
-                <button className="btn btn-success">
-                    Submit
-                </button>
+                    ></textarea>
+                </div>
+                <div className="text-end">
+                    <button className="btn btn-success">
+                        Submit
+                    </button>
+                </div>
             </div>
         </form>
+
     );
 };
 
